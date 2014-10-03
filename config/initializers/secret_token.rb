7ee4d94229aac93dfae057af92bd0e1e297ac57a
buddhamagnet <buddhamagnet@gmail.com>
2014-10-03 03:41:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wildearth::Application.config.secret_token = 'fabd8bf9cf5da4d5e6318bea990fc9e7fccb50cef778253f9f2af9b7e1fe97fab2d282eedf708fba80978cb648ff98379170b2ced6b8f61ad60eef1911f15a91'
