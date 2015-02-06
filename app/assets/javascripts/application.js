a1c004589a98c7b2ee52198b3053a2f41d2c907e
buddhamagnet <buddhamagnet@gmail.com>
2015-02-06 21:01:00 +0000

    showreels

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,3 +28,12 @@ $(function(){
 	$('nav li.selected').removeClass('selected');
     $('nav li a[href="' + window.location.pathname + '"]').parent().addClass('selected');
 });
+
+$(function() {
+  $("#kevin").click(function() {
+    $("#video").hide().html('<iframe width="560" height="315" src="https://www.youtube.com/embed/Q33Xlbmo28k?autoplay=1" frameborder="0" allowfullscreen></iframe>').fadeIn(300);
+  });
+  $("#anders").click(function() {
+    $("#video").hide().html('<iframe width="560" height="315" src="https://www.youtube.com/embed/69rnVurhGFY?autoplay=1" frameborder="0" allowfullscreen></iframe>').fadeIn(300);
+  });
+});
